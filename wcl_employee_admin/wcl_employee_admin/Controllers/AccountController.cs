6e0eda5c39169abd3dc1a891c4ebf742e6f0cff6
hanhT <gm.tranhuuhanh@gmail.com>
2023-07-28 17:31:13 -0700

    TO cover worker

--- wcl_employee_admin/wcl_employee_admin/Controllers/AccountController.cs
+++ wcl_employee_admin/wcl_employee_admin/Controllers/AccountController.cs
@@ -46,6 +46,39 @@ namespace wcl_employee_admin.Controllers
             }
         }
 
+        [HttpGet("GetCoWorkerList")]
+        [Authorize]
+        public async Task<IActionResult> GetCoWorkerAccount()
+        {
+            try
+            {
+                var list = await accountRepo.GetAllAccountAsync();
+                var userList = list.Select((acc, index) => new { username = acc.Username, mail = acc.Email });
+                return Ok(userList.OrderBy(ob => ob.username));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("GetManagerList")]
+        [Authorize]
+        public async Task<IActionResult> GetManagerAccount()
+        {
+            try
+            {
+                var list = await accountRepo.GetAllAccountAsync();
+                var managerList = list.Where(model => model.Position == "CEO" || model.Position == "Operator" || model.Position == "Manager" || model.Position == "Lead").ToList();
+                var managerListCheck = managerList.Select((acc, index) => new { username = acc.Username, mail = acc.Email });
+                return Ok(managerListCheck.OrderBy(ob => ob.username));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
         [HttpGet("GetUserList/{Group}")]
         public async Task<IActionResult> GetAccountByGroup(string Group)
         {
@@ -59,38 +92,12 @@ namespace wcl_employee_admin.Controllers
                 return BadRequest();
             }
         }
-    
 
-
-    [HttpGet("GetUserDetail/{Username}")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "HR")]
-    public async Task<IActionResult> GetAccountByUsername(string Username)
-    {
-        try
+        [HttpGet("GetUserDetail/{Username}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "HR")]
+        public async Task<IActionResult> GetAccountByUsername(string Username)
         {
-            var userDetail = await accountRepo.GetAccountAsync(Username);
-            if (userDetail.Photourl != null)
-            {
-                string baseURL = configuration.GetSection("JWT").GetSection("ValidIssuer").Value;
-                //userDetail.Photos = userDetail.Photourl ;
-                userDetail.Photourl = baseURL + "/ProfileImg/" + userDetail.Username + "/" + userDetail.Photourl;
-
-            }
-            return Ok(userDetail);
-        }
-        catch
-        {
-            return BadRequest();
-        }
-    }
-
-    [HttpGet("GetUserDetail_notHR/{Username}")]
-    [Authorize]
-    public async Task<IActionResult> GetAccountByUsername_user(string Username)
-    {
-        try
-        {
-            if (Username == User.FindFirst(ClaimTypes.Name)?.Value)
+            try
             {
                 var userDetail = await accountRepo.GetAccountAsync(Username);
                 if (userDetail.Photourl != null)
@@ -98,98 +105,122 @@ namespace wcl_employee_admin.Controllers
                     string baseURL = configuration.GetSection("JWT").GetSection("ValidIssuer").Value;
                     //userDetail.Photos = userDetail.Photourl ;
                     userDetail.Photourl = baseURL + "/ProfileImg/" + userDetail.Username + "/" + userDetail.Photourl;
+
                 }
                 return Ok(userDetail);
             }
-            else return BadRequest();
-        }
-        catch
-        {
-            return BadRequest();
-        }
-    }
-
-
-    [HttpPut("UpdateUserDetail/{ID}")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "HR")]
-    public async Task<IActionResult> UpdateAccountByID([FromForm] SignUpModel editmodal, string ID)
-    {
-        try
-        {
-            var result = await accountRepo.UpdateAccountAsync(editmodal, ID);
-            return Ok(result);
-        }
-        catch
-        {
-            return BadRequest();
-        }
-    }
-
-
-
-    [HttpPost("SignUp")]
-
-    public async Task<IActionResult> SignUp([FromForm] SignUpModel signUpModel)
-    {
-        try
-        {
-            var result = await accountRepo.SignUpAsync(signUpModel);
-            if (!result.Action_Result)
+            catch
             {
-                return Unauthorized(result);
+                return BadRequest();
             }
-            return Ok(result);
         }
-        catch
+
+        [HttpGet("GetUserDetail_notHR/{Username}")]
+        [Authorize]
+        public async Task<IActionResult> GetAccountByUsername_user(string Username)
         {
-            return BadRequest();
-        }
-    }
-
-    [HttpPost("SignIn")]
-    public async Task<IActionResult> SignIn(SignInModel signInModel)
-    {
-
-        var result = await accountRepo.SignInAsync(signInModel);
-        if (string.IsNullOrEmpty(result.Message))
-        {
-            return Unauthorized();
-        }
-        return Ok(result);
-    }
-
-
-
-    [HttpPost("UserChangePassWord")]
-    [Authorize]
-    public async Task<IActionResult> UserChangePassword(ChangePassModal model)
-    {
-        var UserNameClaim = User.FindFirst(ClaimTypes.Name)?.Value;
-        if (UserNameClaim != null)
-        {
-            if (UserNameClaim == model.Username)
+            try
             {
-                var result = await accountRepo.UserChangePasswordAsync(model);
-                if ((result.Action_Result) == false)
+                if (Username == User.FindFirst(ClaimTypes.Name)?.Value)
                 {
-                    return Unauthorized();
+                    var userDetail = await accountRepo.GetAccountAsync(Username);
+                    if (userDetail.Photourl != null)
+                    {
+                        string baseURL = configuration.GetSection("JWT").GetSection("ValidIssuer").Value;
+                        //userDetail.Photos = userDetail.Photourl ;
+                        userDetail.Photourl = baseURL + "/ProfileImg/" + userDetail.Username + "/" + userDetail.Photourl;
+                    }
+                    return Ok(userDetail);
+                }
+                else return BadRequest();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpPut("UpdateUserDetail/{ID}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "HR")]
+        public async Task<IActionResult> UpdateAccountByID([FromForm] SignUpModel editmodal, string ID)
+        {
+            try
+            {
+                var result = await accountRepo.UpdateAccountAsync(editmodal, ID);
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+
+
+        [HttpPost("SignUp")]
+
+        public async Task<IActionResult> SignUp([FromForm] SignUpModel signUpModel)
+        {
+            try
+            {
+                var result = await accountRepo.SignUpAsync(signUpModel);
+                if (!result.Action_Result)
+                {
+                    return Unauthorized(result);
                 }
                 return Ok(result);
             }
+            catch
+            {
+                return BadRequest();
+            }
         }
-        return Unauthorized();
+
+        [HttpPost("SignIn")]
+        public async Task<IActionResult> SignIn(SignInModel signInModel)
+        {
+
+            var result = await accountRepo.SignInAsync(signInModel);
+            if (string.IsNullOrEmpty(result.Message))
+            {
+                return Unauthorized();
+            }
+            return Ok(result);
+        }
+
+
+
+        [HttpPost("UserChangePassWord")]
+        [Authorize]
+        public async Task<IActionResult> UserChangePassword(ChangePassModal model)
+        {
+            var UserNameClaim = User.FindFirst(ClaimTypes.Name)?.Value;
+            if (UserNameClaim != null)
+            {
+                if (UserNameClaim == model.Username)
+                {
+                    var result = await accountRepo.UserChangePasswordAsync(model);
+                    if ((result.Action_Result) == false)
+                    {
+                        return Unauthorized();
+                    }
+                    return Ok(result);
+                }
+            }
+            return Unauthorized();
+        }
+
+
+        [HttpDelete("DeletePassWord")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "HR")]
+        public async Task<IActionResult> DeletePassword(DisableAccModal model)
+        {
+            var result = await accountRepo.DeleteAcountAsync(model);
+
+            return Ok(result);
+        }
+
+
     }
-
-
-    [HttpDelete("DeletePassWord")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "HR")]
-    public async Task<IActionResult> DeletePassword(DisableAccModal model)
-    {
-        var result = await accountRepo.DeleteAcountAsync(model);
-
-        return Ok(result);
-    }
-
-
-}
 }
