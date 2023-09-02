24aefc0ae2a79dd89a3721cc2258a98c2b6b0739
hanhT <gm.tranhuuhanh@gmail.com>
2023-09-01 17:30:24 -0700

    bug add DKP

---
+++ wcl_employee_admin/wcl_employee_admin/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wcl_employee_admin.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
